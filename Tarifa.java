136facbf0605a6bbb576a718586a2e6cf9a5b06d
Agustin Cuevas <135286534+IntAgustinCuevas@users.noreply.github.com>
2024-11-12 14:21:10 -0300

    Add files via upload

---
+++ Tarifa.java
@@ -0,0 +1,25 @@
+
+public abstract class Tarifa {
+	private float valorKhw;
+	private float iva;
+
+	
+	/* constructor */
+	
+	/* getters && setters */
+	public float getValorKhw() {
+		return valorKhw;
+	}
+	public void setValorKhw(float valorKhw) {
+		this.valorKhw = valorKhw;
+	}
+	public float getIva() {
+		return iva;
+	}
+	public void setIva(float iva) {
+		this.iva = iva;
+	}
+	
+	/* metodos */
+	public abstract float calcularTarifa(int consumo);
+}
